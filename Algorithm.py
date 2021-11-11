9a0d4384779c694b8262f727b3a3c74479f219de
AsmitaKhaitan <68748068+AsmitaKhaitan@users.noreply.github.com>
2021-11-11 18:44:41 +0530

    Add files via upload
    A function to add a random2 or 4 is added to a random blank space in the grid

--- Algorithm.py
+++ Algorithm.py
@@ -17,3 +17,20 @@ def start():
     print("Press '3' to MOVE UP")
     print("Press '4' to MOVE DOWN")
 
+    add_num(board)
+    return board
+
+# function to add new 2 or 4 to the board at any random cell
+def add_num(board):
+    #choosing random row and column
+    r= random.rnadint(0,3)
+    c= random.rnadint(0,3)
+
+    while(board[r]!=0):
+        r= random.rnadint(0,3)
+        c= random.rnadint(0,3)
+
+    #the while loop breaks art a position where the board is empty (0)
+    #the following adds a random 2 or 4 to the empty cell
+    list1 = [2,4]
+    board[r] = random.choice(list1)
