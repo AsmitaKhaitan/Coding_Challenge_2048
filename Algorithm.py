b88245cd526220b35cce15874ad31c4ea38d717e
AsmitaKhaitan <68748068+AsmitaKhaitan@users.noreply.github.com>
2021-11-11 18:57:36 +0530

    Add files via upload
    A function state is added to check the state of the game, that is, if the player has won or lost or if the game is still unfinished

--- Algorithm.py
+++ Algorithm.py
@@ -30,7 +30,36 @@ def add_num(board):
         r= random.rnadint(0,3)
         c= random.rnadint(0,3)
 
-    #the while loop breaks art a position where the board is empty (0)
+    #the while loop breaks at a position where the board is empty (0)
     #the following adds a random 2 or 4 to the empty cell
     list1 = [2,4]
     board[r] = random.choice(list1)
+
+
+#function to check state of game (won/lost/unfinished)
+def state(board):
+
+    #goal of the game is to reach the number 2048
+    for i in range(4):
+        for j in range(4):
+            if(board[i][j]==2048):
+                return '!!CONGRATULATIONS!! YOU WON!!'
+
+    #if even one of the cells is empty (0) then the game is not over
+    for i in range(4):
+        for j in range(4):
+            if(board[i][j]==0):
+                return 'CONTINUE PLAYING, THE GAME IS NOT YET OVER!!!'
+    #if no space is empty but after any move two or more cells can merge then the game is not over yet
+    for i in range (3):
+        for j in range(3):
+            if(board[i][j]==board[i+1][j] or board[i][j]==board[i][j+1]):
+                return 'CONTINUE PLAYING, THE GAME IS NOT YET OVER!!!'
+    for k in range(3):
+        if(board[3][k]==board[3][k+1]):
+            return 'CONTINUE PLAYING, THE GAME IS NOT YET OVER!!!'
+    for i in range(3):
+        if(board[i][3]==board[i+1][3]):
+            return 'CONTINUE PLAYING, THE GAME IS NOT YET OVER!!!'
+
+    return 'SORRY Y0U LOST !! :('
